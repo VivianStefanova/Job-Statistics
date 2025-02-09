49cee27689b4076bb028c15a8da2b32e702a0b0b
VivianStefanova <vivian.stefanova04@gmail.com>
2025-02-09 17:41:29 +0200

    sync

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 from src.MITM_scraper import get_response as mi
 from src.API_scraper import get_response as api
 import src.data_menager as sql
-import mysql.connector
-# response = get_response(1,10)
-# for i in response:
-#     if("salary_min" in i):
-#         print(i["salary_min"])
-#     else : print("No salary")    
 
-# response = get_response(2,10);
-# for i in response:
-#     if("salary_min" in i):
-#         print(i["salary_min"])
-#     else : print("No salary")   
 
 # response = mi(2)
 # response2 = api(2)
@@ -22,7 +11,10 @@ import mysql.connector
 # print(".........................................................................................")
 # print(response2[6])
 # #print(response)
-sql.start_SQL("py_test", "py_table")
+#sql.start_SQL("py_test", "py_table")
+#data = mi(1,"", 20)
+data1 = api(1, 20)
+sql.insert_values(data1, True, "py_test", "py_table");
 
 
 print("end")
